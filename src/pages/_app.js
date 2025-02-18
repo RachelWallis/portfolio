7a0c35c4f90173b4d39f819540639d6b65ddc878
Rachel Wallis <rachelwalliswork@gmail.com>
2025-02-18 18:44:12 +0000

    fixed favicon path

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -17,7 +17,7 @@ function MyApp({ Component, pageProps }) {
           {/* seo begin */}
           <title>{appData.settings.siteName}</title>
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <link rel="icon" href="/favicon.svg" />
+          <link rel="icon" href="/img/favicon.svg" />
           {/* seo end */}        
       </Head>
       <Component {...pageProps} />
